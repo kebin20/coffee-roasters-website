c49d3bfc1e88f777468f649125f5aad63effc536
kebin20 <ktanzyl@hotmail.com>
2023-02-07 08:56:12 +0900

    style: added how it works section, attempting to fix layout for why choose us

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import styles from "./Home.module.css";
-import MainButton from "../UI/MainButton";
-import NavBar from "../UI/NavBar";
+import React from 'react';
+import styles from './Home.module.css';
+import MainButton from '../UI/MainButton';
+import NavBar from '../UI/NavBar';
 
 /* Images */
-import granEspresso from "../assets/home/desktop/image-gran-espresso.png";
-import planalto from "../assets/home/desktop/image-planalto.png";
-import piccollo from "../assets/home/desktop/image-piccollo.png";
-import danche from "../assets/home/desktop/image-danche.png";
-import coffeeBean from "../assets/home/desktop/icon-coffee-bean.svg";
-import gift from "../assets/home/desktop/icon-gift.svg";
-import truck from "../assets/home/desktop/icon-truck.svg";
+import granEspresso from '../assets/home/desktop/image-gran-espresso.png';
+import planalto from '../assets/home/desktop/image-planalto.png';
+import piccollo from '../assets/home/desktop/image-piccollo.png';
+import danche from '../assets/home/desktop/image-danche.png';
+import coffeeBean from '../assets/home/desktop/icon-coffee-bean.svg';
+import gift from '../assets/home/desktop/icon-gift.svg';
+import truck from '../assets/home/desktop/icon-truck.svg';
 
 export default function Home() {
   return (
@@ -89,46 +89,76 @@ export default function Home() {
               level.
             </p>
           </div>
-          <div className={styles.whyUsFeaturesBgContainer}>
-            <div className={styles.whyUsFeaturesContainer}>
-              <div className={styles.featuresItem}>
-                <img
-                  className={styles.featuresImage}
-                  src={coffeeBean}
-                  alt="coffee bean icon"
-                />
-                <h2>Best Quality</h2>
-                <p>
-                  Discover an endless variety of the world’s best artisan coffee
-                  from each of our roasters.
-                </p>
-              </div>
-              <div className={styles.featuresItem}>
-                <img
-                  className={styles.featuresImage}
-                  src={gift}
-                  alt="gift icon"
-                />
-                <h2>Exclusive Benefits</h2>
-                <p>
-                  Special offers and swag when you subscribe, including 30% off
-                  your first shipment.
-                </p>
-              </div>
-              <div className={styles.featuresItem}>
-                <img
-                  className={styles.featuresImage}
-                  src={truck}
-                  alt="truck icon"
-                />
-                <h2>Free Shipping</h2>
-                <p>
-                  We cover the cost and coffee is delivered fast. Peak
-                  freshness: guaranteed.
-                </p>
-              </div>
+          <div className={styles.whyUsFeaturesContainer}>
+            <div className={styles.featuresItem}>
+              <img
+                className={styles.featuresImage}
+                src={coffeeBean}
+                alt="coffee bean icon"
+              />
+              <h2>Best Quality</h2>
+              <p>
+                Discover an endless variety of the world’s best artisan coffee
+                from each of our roasters.
+              </p>
+            </div>
+            <div className={styles.featuresItem}>
+              <img
+                className={styles.featuresImage}
+                src={gift}
+                alt="gift icon"
+              />
+              <h2>Exclusive Benefits</h2>
+              <p>
+                Special offers and swag when you subscribe, including 30% off
+                your first shipment.
+              </p>
+            </div>
+            <div className={styles.featuresItem}>
+              <img
+                className={styles.featuresImage}
+                src={truck}
+                alt="truck icon"
+              />
+              <h2>Free Shipping</h2>
+              <p>
+                We cover the cost and coffee is delivered fast. Peak freshness:
+                guaranteed.
+              </p>
             </div>
           </div>
+        </section>
+
+        <section className={styles.howItWorks}>
+          <h1 className={styles.howItWorksTitle}>How it works</h1>
+          <div className={styles.steps}>
+            <h1 className={styles.stepTitle}>01</h1>
+            <h2>Pick your coffee</h2>
+            <p>
+              Select from our evolving range of artisan coffees. Our beans are
+              ethically sourced and we pay fair prices for them. There are new
+              coffees in all profiles every month for you to try out.
+            </p>
+          </div>
+          <div className={styles.steps}>
+            <h1 className={styles.stepTitle}>01</h1>
+            <h2>Pick your coffee</h2>
+            <p>
+              Select from our evolving range of artisan coffees. Our beans are
+              ethically sourced and we pay fair prices for them. There are new
+              coffees in all profiles every month for you to try out.
+            </p>
+          </div>
+          <div className={styles.steps}>
+            <h1 className={styles.stepTitle}>01</h1>
+            <h2>Pick your coffee</h2>
+            <p>
+              Select from our evolving range of artisan coffees. Our beans are
+              ethically sourced and we pay fair prices for them. There are new
+              coffees in all profiles every month for you to try out.
+            </p>
+          </div>
+          <MainButton>Create your plan</MainButton>
         </section>
       </main>
     </>
