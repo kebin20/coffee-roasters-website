fc051c999d4d25963a51a79f1ee3a23ef5eac6de
kebin20 <ktanzyl@hotmail.com>
2023-02-08 08:56:56 +0900

    style: Completed styling for our commitment section

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,6 +3,9 @@ import styles from './AboutUs.module.css';
 import NavBar from '../UI/NavBar';
 import Footer from '../UI/Footer';
 
+/* Images */
+import commitment from '../assets/about/mobile/image-commitment.jpg';
+
 export default function AboutUs() {
   return (
     <>
@@ -15,6 +18,24 @@ export default function AboutUs() {
             highlighting stories of coffee from around the world. We have since
             been dedicated to bring the perfect cup - from bean to brew - in
             every shipment.
+          </p>
+        </section>
+
+        <section className={styles.ourCommitment}>
+          <img className={styles.ourCommitmentImage} src={commitment} alt="picture of a guy pouring coffee" />
+          <h1>Our commitment</h1>
+          <p>
+            We’re built on a simple mission and a commitment to doing good along
+            the way. We want to make it easy for you to discover and brew the
+            world’s best coffee at home. It all starts at the source. To locate
+            the specific lots we want to purchase, we travel nearly 60 days a
+            year trying to understand the challenges and opportunities in each
+            of these places. We collaborate with exceptional coffee growers and
+            empower a global community of farmers through with well above
+            fair-trade benchmarks. We also offer training, support farm
+            community initiatives, and invest in coffee plant science. Curating
+            only the finest blends, we roast each lot to highlight tasting
+            profiles distinctive to their native growing region.
           </p>
         </section>
       </main>
