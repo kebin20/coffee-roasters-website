57b6d939c1e27efae3dba0269db853660d31fd42
kebin20 <ktanzyl@hotmail.com>
2023-02-09 09:13:15 +0900

    feat: created mockup code for making dropdown menu

--- src/Pages/Subscribe.jsx
+++ src/Pages/Subscribe.jsx
@@ -47,6 +47,76 @@ export default function Subscribe() {
           </p>
         </div>
       </section>
+
+      <section className={styles.customizePlanContainer}>
+      {/* <div class="dropdowns" id="dropdowns">
+          <div class={styles.howDoYouDrink-container} nav-item">
+            <div class="dropdown" data-dropdown-product>
+              <button
+                class="link dropbtn"
+                id="drop-btn"
+                data-dropdown-button-product
+              >
+                How do you drink your coffee?
+                <svg
+                  class="arrow arrow-down"
+                  id="arrow-product"
+                  width="10"
+                  height="6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  data-dropdown-button-product
+                >
+                  <path
+                    stroke="none"
+                    stroke-width="1.5"
+                    fill="none"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </svg>
+              </button>
+              <div class="dropdown-menu product-menu" id="dropdownMenu">
+                <a href="#" class="link nav-link">Overview</a>
+                <a href="#" class="link nav-link"> Pricing</a>
+                <a href="#" class="link nav-link">Features</a>
+                <a href="#" class="link nav-link">Integrations</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="company-container nav-item">
+            <div class="dropdown" data-dropdown-company>
+              <button
+                class="link dropbtn"
+                id="drop-btn"
+                data-dropdown-button-company
+              >
+                Company
+                <svg
+                  class="arrow arrow-down"
+                  id="arrow-company"
+                  width="10"
+                  height="6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  data-dropdown-button-company
+                >
+                  <path
+                    stroke="none"
+                    stroke-width="1.5"
+                    fill="none"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+              <div class="dropdown-menu company-menu" id="dropdownMenu">
+                <a href="#" class="link nav-link">About</a>
+                <a href="#" class="link nav-link">Team</a>
+                <a href="#" class="link nav-link">Blog</a>
+                <a href="#" class="link nav-link">Careers</a>
+              </div>
+            </div>
+          </div> */}
+      </section>
     </>
   );
 }
