df98c9bb87cd3bab67a8b64b9df9b1e7449d41ba
kebin20 <ktanzyl@hotmail.com>
2023-02-14 08:56:24 +0900

    chore: added data file to hold content for dynamic output

---
+++ src/planData.jsx
@@ -0,0 +1,54 @@
+const plan = [
+  {
+    id: 1,
+    menu: 'How do you drink your coffee?',
+    titles: ['Capsule', 'Filter', 'Espresso'],
+    descriptions: [
+      'Compatible with Nespresso systems and similar brewers',
+      'For pour over or drip methods like Aeropress, Chemex, and V60',
+      'Dense and finely ground beans for an intense, flavorful experience',
+    ],
+  },
+  {
+    id: 2,
+    menu: 'What type of coffee?',
+    titles: ['Single Origin', 'Decaf', 'Blended'],
+    descriptions: [
+      'Distinct, high quality coffee from a specific family-owned farm',
+      'Just like regular coffee, except the caffeine has been removed',
+      'Combination of two or three dark roasted beans of organic coffees',
+    ],
+  },
+  {
+    id: 3,
+    menu: 'How much would you like?',
+    titles: ['250g', '500g', '1000g'],
+    descriptions: [
+      'Perfect for the solo drinker. Yields about 12 delicious cups.',
+      'Perfect option for a couple. Yields about 40 delectable cups.',
+      'Perfect for offices and events. Yields about 90 delightful cups.',
+    ],
+  },
+  {
+    id: 4,
+    menu: 'Want us to grind them?',
+    titles: ['Wholebean', 'Filter', 'Cafetiére'],
+    descriptions: [
+      'Best choice if you cherish the full sensory experience',
+      'For drip or pour-over coffee methods such as V60 or Aeropress',
+      'Course ground beans specially suited for french press coffee',
+    ],
+  },
+  {
+    id: 5,
+    menu: 'How often should we deliver?',
+    titles: ['Every week', 'Every 2 weeks', 'Every month'],
+    descriptions: [
+      '$7.20 per shipment. Includes free first-class shipping.',
+      '$9.60 per shipment. Includes free priority shipping.',
+      '$12.00 per shipment. Includes free priority shipping.',
+    ],
+  },
+];
+
+export default plan;
