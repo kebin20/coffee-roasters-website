540c16e22f6a08a9cff0c2d085b2668a31542982
kebin20 <ktanzyl@hotmail.com>
2023-02-16 10:32:00 +0900

    feat: able to save value of button from plan item

--- src/UI/PlanDropdownItem.jsx
+++ src/UI/PlanDropdownItem.jsx
@@ -3,11 +3,17 @@ import styles from './PlanDropdownItem.module.css';
 import PropTypes from 'prop-types';
 
 export default function PlanDropdownItem(props) {
+  function savePlanTitle(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+  }
+
   return (
     <>
       <button
         className={styles.dropDownItem}
-        onClick={() => console.log('Clicked!')}
+        onClick={savePlanTitle}
+        value={props.content.title}
       >
         <span className={styles.buttonTitle}>{props.content.title}</span> <br />
         <span className={styles.buttonText}>{props.content.description}</span>
