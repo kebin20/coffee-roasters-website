f942675315f512076890a1b7de2ff1364a716214
kebin20 <ktanzyl@hotmail.com>
2023-02-16 21:25:30 +0900

    style: styled quality container and adjusted sizing and layout for commitment

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -60,7 +60,7 @@ export default function AboutUs() {
           </section>
 
           <section className={styles.qualityContainer}>
-            <div className={styles.imageWrapper}>
+            <div className={styles.qualityImageWrapper}>
               <picture className={styles.qualityImageLg}>
                 <source srcSet={lgQuality} media="(min-width: 1200px)" />
                 <img
@@ -70,15 +70,17 @@ export default function AboutUs() {
                 />
               </picture>
             </div>
-            <h1 className={styles.qualityTitle}>Uncomprimising quality</h1>
-            <p>
-              Although we work with growers who pay close attention to all
-              stages of harvest and processing, we employ, on our end, a
-              rigorous quality control program to avoid over-roasting or baking
-              the coffee dry. Every bag of coffee is tagged with a roast date
-              and batch number. Our goal is to roast consistent, user-friendly
-              coffee, so that brewing is easy and enjoyable.
-            </p>
+            <div className={styles.qualityContentWrapper}>
+              <h1 className={styles.qualityTitle}>Uncomprimising quality</h1>
+              <p>
+                Although we work with growers who pay close attention to all
+                stages of harvest and processing, we employ, on our end, a
+                rigorous quality control program to avoid over-roasting or
+                baking the coffee dry. Every bag of coffee is tagged with a
+                roast date and batch number. Our goal is to roast consistent,
+                user-friendly coffee, so that brewing is easy and enjoyable.
+              </p>
+            </div>
           </section>
 
           <section className={styles.hqContainer}>
