12222552d25fa216c29e0c4139aafbe7330e4fb2
kebin20 <ktanzyl@hotmail.com>
2023-02-26 20:08:05 +0900

    build: installed TypScript dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+// vite.config.js
+import checker from "vite-plugin-checker";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+    reactRefresh(),
+  ],
+  // esbuild: {
+  //   jsxFactory: "React.createElement",
+  //   jsxFragment: "React.Fragment",
+  // },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
