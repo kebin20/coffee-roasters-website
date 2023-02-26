de85b2f3ab91af3495594c7eabfc3204538c04f2
kebin20 <ktanzyl@hotmail.com>
2023-02-27 07:43:14 +0900

    feat: added new coffee page to display common coffee types

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import AboutUs from './Pages/AboutUs';
 import Subscribe from './Pages/Subscribe';
+import Coffee from './Pages/Coffee';
 
 export default function App() {
+  const [coffee, setCoffee] = useState([]);
+
+  function fetchCoffeeDescription() {
+    fetch('https://api.sampleapis.com/coffee/hot')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => console.log(data));
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/subscribe" element={<Subscribe />} />
+        <Route path="/coffee" element={<Coffee />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/Coffee.jsx
@@ -0,0 +1,11 @@
+import styles from "./Coffee.module.css"
+import { Link } from "react-router-dom"
+
+export default Coffee (props) {
+  return (
+    <li>
+      <h2></h2>
+      <p></p>
+    </li>
+  )
+}
