fd5d936182754db37fadbac6ea0d6f6fffe92b07
kebin20 <ktanzyl@hotmail.com>
2023-02-28 07:22:21 +0900

    refactor: moved business logic of data fetch to Coffee component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './Pages/Home';
-import AboutUs from './Pages/AboutUs';
-import Subscribe from './Pages/Subscribe';
-import Coffee from './Pages/Coffee';
+import Home from "./Pages/Home";
+import AboutUs from "./Pages/AboutUs";
+import Subscribe from "./Pages/Subscribe";
+import Coffee from "./Pages/Coffee";
 
 export default function App() {
-  const [coffee, setCoffee] = useState([]);
-
-  useEffect(() => {
-    fetch('https://api.sampleapis.com/coffee/hot')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => setCoffee(data.results));
-  }, []);
-
-  console.log(coffee);
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/subscribe" element={<Subscribe />} />
-        <Route path="/coffee" element={<Coffee coffee={coffee} />} />
+        <Route path="/coffee" element={<Coffee />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Coffee.jsx
+++ src/Pages/Coffee.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import styles from './Coffee.module.css';
-import Wrapper from '../UI/Wrapper';
-import NavBar from '../Layout/NavBar';
-import Footer from '../Layout/Footer';
+import React from "react";
+import styles from "./Coffee.module.css";
+import Wrapper from "../UI/Wrapper";
+import NavBar from "../Layout/NavBar";
+import Footer from "../Layout/Footer";
 
 export default function Coffee(props) {
+  const [coffee, setCoffee] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://api.sampleapis.com/coffee/hot")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => setCoffee(data.results));
+    setIsLoading(false);
+  }, []);
+
+  console.log(coffee);
+
   return (
     <Wrapper>
       <NavBar />
-      <li>
-        <h2>Test</h2>
-        <p>Test</p>
-      </li>
+      {!isLoading && coffee.length > 0 && (
+        <li>
+          <h2>{data.results}</h2>
+          <p>Test</p>
+        </li>
+      )}
+      {!isLoading && coffee.length === 0 && <p>No coffeee</p>}
+      {isLoading && <p>Loading...</p>}
       <Footer />
     </Wrapper>
   );
