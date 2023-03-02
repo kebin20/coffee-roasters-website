bd30a6de884c17749420035f9edece9b03c4e34a
kebin20 <ktanzyl@hotmail.com>
2023-03-02 13:23:43 +0900

    fix: added alt text for coffee image not loading and a backup src for the image

--- src/Pages/Coffee.jsx
+++ src/Pages/Coffee.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Coffee.module.css';
+import styles from "./Coffee.module.css";
 
 export default function Coffee(props) {
   return (
     <li className={styles.coffee}>
-      <img src={props.image} alt="" className={styles.coffeeImages}/>
+      <img
+        srcSet={props.image}
+        src="https://medellinliving.com/wp-content/uploads/2022/08/Coffee-Shopping-Guide-Whats-the-Best-Colombian-Coffee-Brand.png"
+        alt="picture of coffee"
+        className={styles.coffeeImages}
+      />
       <h2>{props.title}</h2>
       <p className={styles.description}>{props.description}</p>
     </li>
