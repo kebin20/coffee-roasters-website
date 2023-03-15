0db771c48b37e5ccfbb9c8aa6d127ae0e5698dff
kebin20 <ktanzyl@hotmail.com>
2023-03-16 08:58:38 +0900

    feat: successfully implemented price updates according to weight of coffee beans shown

--- src/components/Selections/Delivery.jsx
+++ src/components/Selections/Delivery.jsx
@@ -8,7 +8,7 @@ export default function Delivery(props) {
   const { content, menu } = props.plan;
 
   const [open, setOpen] = useState(true);
-  const [conditionalContent, setConditionalContent] = useState();
+  const [description, setDescription] = useState({});
 
   const { firstWeight, secondWeight, thirdWeight } = props.weight;
 
@@ -17,44 +17,86 @@ export default function Delivery(props) {
     props.onButtonClick(event.target.value);
   }
 
-  function createConditionalContent() {
-    let newContent;
-    if (secondWeight) {
-      const splitContentArray = content[0].description.split(' ');
-      splitContentArray.splice(0, 1, '$13.00');
-      const joinedFirstContent = splitContentArray.join(' ');
-      newContent = joinedFirstContent;
-    } else if (thirdWeight && !secondWeight) {
-      const splitContentArray = content[0].description.split(' ');
-      splitContentArray.splice(0, 1, '$22.00');
-      const joinedSecondContent = splitContentArray.join(' ');
-      newContent = joinedSecondContent;
+  function createConditionalDescription() {
+    let modWeekDescription;
+    let modFortnightDescription;
+    let modMonthDescription;
+
+    if (secondWeight && !firstWeight && !thirdWeight) {
+      const splitWeekDescriptionArr = content[0].description.split(' ');
+      const splitFortnightDescriptionArr = content[1].description.split(' ');
+      const splitMonthDescriptionArr = content[2].description.split(' ');
+      splitWeekDescriptionArr.splice(0, 1, '$13.00');
+      splitFortnightDescriptionArr.splice(0, 1, '$17.50');
+      splitMonthDescriptionArr.splice(0, 1, '$22.00');
+      const joinedFirstWeekDescription = splitWeekDescriptionArr.join(' ');
+      const joinedFirstFortnightDescription =
+        splitFortnightDescriptionArr.join(' ');
+      const joinedFirstMonthDescription = splitMonthDescriptionArr.join(' ');
+      modWeekDescription = joinedFirstWeekDescription;
+      modFortnightDescription = joinedFirstFortnightDescription;
+      modMonthDescription = joinedFirstMonthDescription;
+    } else if (thirdWeight && !secondWeight && !firstWeight) {
+      const splitWeekDescriptionArr = content[0].description.split(' ');
+      const splitFortnightDescriptionArr = content[1].description.split(' ');
+      const splitMonthDescriptionArr = content[2].description.split(' ');
+      splitWeekDescriptionArr.splice(0, 1, '$22.00');
+      splitFortnightDescriptionArr.splice(0, 1, '$32.00');
+      splitMonthDescriptionArr.splice(0, 1, '$42.00');
+      const joinedSecondWeekDescription = splitWeekDescriptionArr.join(' ');
+      const joinedSecondFortnightDescription =
+        splitFortnightDescriptionArr.join(' ');
+      const joinedSecondMonthDescription = splitMonthDescriptionArr.join(' ');
+      modWeekDescription = joinedSecondWeekDescription;
+      modFortnightDescription = joinedSecondFortnightDescription;
+      modMonthDescription = joinedSecondMonthDescription;
     } else {
-      newContent = content[0].description;
+      modWeekDescription = content[0].description;
+      modFortnightDescription = content[1].description;
+      modMonthDescription = content[2].description;
     }
-    setConditionalContent(newContent);
+    setDescription({
+      week: modWeekDescription,
+      fortnight: modFortnightDescription,
+      month: modMonthDescription,
+    });
   }
 
   useEffect(() => {
-    createConditionalContent();
+    createConditionalDescription();
   }, [props.weight]);
 
-  console.log(conditionalContent);
-
-  const dropdownItems = content.map((item) => {
-    return (
+  const dropdownItems = (
+    <>
       <PlanDropdownItem
-        key={item.id}
         onClick={(event) => {
           props.onHoldChoice(item.id, event);
           handleClick(event);
         }}
-        title={item.title}
-        description={item.description}
-        isSelected={item.isSelected}
+        title={content[0].title}
+        description={description.week}
+        isSelected={content[0].isSelected}
       />
-    );
-  });
+      <PlanDropdownItem
+        onClick={(event) => {
+          props.onHoldChoice(item.id, event);
+          handleClick(event);
+        }}
+        title={content[1].title}
+        description={description.fortnight}
+        isSelected={content[1].isSelected}
+      />
+      <PlanDropdownItem
+        onClick={(event) => {
+          props.onHoldChoice(item.id, event);
+          handleClick(event);
+        }}
+        title={content[2].title}
+        description={description.month}
+        isSelected={content[2].isSelected}
+      />
+    </>
+  );
 
   return (
     <li className={styles.selectionItem}>
