a3a1ccad8e18627172141e8302feff0b2d1718c6
kebin20 <ktanzyl@hotmail.com>
2023-03-16 09:47:49 +0900

    style: adjust class naming and styling for order modal

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styles from "./OrderModal.module.css";
-import MainButton from "../UI/MainButton";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './OrderModal.module.css';
+import MainButton from '../UI/MainButton';
 
 export default function OrderModal(props) {
   const { coffeeMethod, coffeeType, amount, grindType, delivery } =
@@ -12,17 +12,20 @@ export default function OrderModal(props) {
       <div className={styles.orderModal}>
         <h1 className={styles.orderModalTitle}>Order Summary</h1>
         <div className={styles.orderModalSummaryWrapper}>
-          <p className={styles.orderModal__p}>
-            “I drink my coffee {props.isCapsule ? " using " : " as "}
-            <span className={styles.orderModal__span}>{coffeeMethod}</span>,
-            with a <span className={styles.orderModal__span}>{coffeeType}</span>{" "}
-            type of bean.{" "}
-            <span className={styles.orderModal__span}>{amount}</span> ground{" "}
+          <p className={styles.orderModalSummary}>
+            “I drink my coffee {props.isCapsule ? ' using ' : ' as '}
+            <span className={styles.orderModalSpan}>{coffeeMethod}</span>, with
+            a <span className={styles.orderModalSpan}>{coffeeType}</span> type
+            of bean. <span className={styles.orderModalSpan}>{amount}</span>{' '}
+            ground{' '}
             {!props.isCapsule && (
-              <span className={styles.orderModal__span}>ala{grindType}</span>
+              <>
+                <span>ala </span>
+                <span className={styles.orderModalSpan}>{grindType}</span>
+              </>
             )}
-            , sent to me{" "}
-            <span className={styles.orderModal__span}>{delivery}</span>.”
+            , sent to me{' '}
+            <span className={styles.orderModalSpan}>{delivery}</span>.”
           </p>
           <p className={styles.doubleCheckOrder}>
             Is this correct? You can proceed to checkout or go back to plan
@@ -37,9 +40,9 @@ export default function OrderModal(props) {
 }
 
 OrderModal.propTypes = {
-  coffeeMethod: PropTypes.string.isRequired,
-  caffeineOption: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  frequency: PropTypes.string.isRequired,
+  coffeeMethod: PropTypes.string,
+  caffeineOption: PropTypes.string,
+  size: PropTypes.string,
+  type: PropTypes.string,
+  frequency: PropTypes.string,
 };
