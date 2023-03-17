b4fb1d0e9be35b6fe704fed064646c11d95d41f7
kebin20 <ktanzyl@hotmail.com>
2023-03-17 11:16:48 +0900

    chore: enabled typescript and setup ts config

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import checker from "vite-plugin-checker";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      // e.g. use TypeScript check
+      typescript: true,
+    }),
+  ],
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
+});
+
+
