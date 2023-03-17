cd2fde6349d22d6828a56ab54c86ff454bce380e
kebin20 <ktanzyl@hotmail.com>
2023-03-17 09:29:54 +0900

    feat: enabled user to close the order modal once the plan has been confirmed

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderModal.module.css';
 import MainButton from '../UI/MainButton';
 
 export default function OrderModal(props) {
+  const [confirmCheckout, setConfirmCheckout] = useState(false);
+
   const { coffeeMethod, coffeeType, amount, grindType, delivery } =
     props.orderContent;
 
-  const { isCapsule, shippingPrice } = props;
+  const { isCapsule, shippingPrice, closeModal } = props;
 
   return (
     <div className={styles.backdrop}>
       <div className={styles.orderModal}>
         <h1 className={styles.orderModalTitle}>Order Summary</h1>
         <div className={styles.orderModalSummaryWrapper}>
-          <p className={styles.orderModalSummary}>
-            “I drink my coffee {props.isCapsule ? ' using ' : ' as '}
-            <span className={styles.orderModalSpan}>{coffeeMethod}</span>, with
-            a <span className={styles.orderModalSpan}>{coffeeType}</span> type
-            of bean. <span className={styles.orderModalSpan}>{amount}</span>{' '}
-            ground{' '}
-            {!isCapsule && (
-              <>
-                <span>ala </span>
-                <span className={styles.orderModalSpan}>{grindType}</span>
-              </>
-            )}
-            , sent to me{' '}
-            <span className={styles.orderModalSpan}>{delivery}</span>.”
-          </p>
-          <p className={styles.doubleCheckOrder}>
-            Is this correct? You can proceed to checkout or go back to plan
-            selection if something is off. Subscription discount codes can also
-            be redeemed at the checkout.
-          </p>
-          <MainButton>Checkout - ${shippingPrice.toFixed(2)}/mo</MainButton>
+          {!confirmCheckout && (
+            <>
+              <p className={styles.orderModalSummary}>
+                “I drink my coffee {props.isCapsule ? ' using ' : ' as '}
+                <span className={styles.orderModalSpan}>{coffeeMethod}</span>,
+                with a{' '}
+                <span className={styles.orderModalSpan}>{coffeeType}</span> type
+                of bean. <span className={styles.orderModalSpan}>{amount}</span>{' '}
+                ground{' '}
+                {!isCapsule && (
+                  <>
+                    <span>ala </span>
+                    <span className={styles.orderModalSpan}>{grindType}</span>
+                  </>
+                )}
+                , sent to me{' '}
+                <span className={styles.orderModalSpan}>{delivery}</span>.”
+              </p>
+              <p className={styles.doubleCheckOrder}>
+                Is this correct? You can proceed to checkout or go back to plan
+                selection if something is off. Subscription discount codes can
+                also be redeemed at the checkout.
+              </p>
+            </>
+          )}
+          {confirmCheckout && (
+            <h2 className={styles.orderModalTitle}>
+              Thank you for your order! Please check out for more information on
+              our range of coffee
+            </h2>
+          )}
+          {!confirmCheckout && (
+            <MainButton onClick={() => setConfirmCheckout(true)}>
+              Checkout - ${shippingPrice.toFixed(2)}/mo
+            </MainButton>
+          )}
+          {confirmCheckout && (
+            <MainButton onClick={closeModal}>Return to menu</MainButton>
+          )}
         </div>
       </div>
     </div>
