42e2b818b1e28e8a27e18e725d7a1c11293a9930
kebin20 <ktanzyl@hotmail.com>
2023-04-29 12:49:42 +0900

    feat: added role to the li element, added ingredients list for each coffee

--- src/components/Coffee.tsx
+++ src/components/Coffee.tsx
@@ -3,10 +3,11 @@ import { CoffeeItemProps } from "../interfaces";
 import styles from "./Coffee.module.css";
 
 export default function Coffee(props: CoffeeItemProps) {
-  const { image, description, title } = props;
+  const { image, description, title, ingredients} = props;
+
 
   return (
-    <li className={styles.coffee}>
+    <li className={styles.coffee} role="listitem">
       <img
         srcSet={image}
         src="https://medellinliving.com/wp-content/uploads/2022/08/Coffee-Shopping-Guide-Whats-the-Best-Colombian-Coffee-Brand.png"
@@ -16,6 +17,14 @@ export default function Coffee(props: CoffeeItemProps) {
       <div className={styles.coffeeItemContent}>
         <h2 className={styles.coffeeItemTitle}>{title}</h2>
         <p className={styles.coffeeItemDescription}>{description}</p>
+        <div className={styles.ingredientsContainer}>
+          <h3>Ingredients:</h3>
+          <ul className={styles.ingredients}>
+            {ingredients.map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
       </div>
     </li>
   );
